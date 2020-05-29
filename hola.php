85bf5ebc9717db348349e87428f49bf5af7852c9
pntkrtr <espilopez@gmail.com>
2020-05-29 21:57:09 +0200

    Se han incluido los dos apellidos

--- hola.php
+++ hola.php
@@ -5,6 +5,6 @@
         <title>Prueba de PHP</title>
     </head>
     <body>
-        <?php echo '<p>Hola soy Victor Manuel</p>'; ?>
+        <?php echo '<p>Hola, soy V&iactute;ctor Manuel Espinosa L&oacute;pez</p>'; ?>
     </body>
 </html>
