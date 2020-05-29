6ced2a3e052794fe126fbcce75f05c398c269ba6
Víctor Manuel Espinosa López <pntkrtr@users.noreply.github.com>
2020-05-29 21:15:27 +0200

    Create hola.php

---
+++ hola.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Prueba de PHP</title>
+    </head>
+    <body>
+        <?php echo '<p>Hola soy Victor Manuel</p>'; ?>
+    </body>
+</html>
